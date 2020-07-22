aca2f942f8f4cc8fac4468ca4e766394a7ba0ea7
Kousika <kousika@Macs-MacBook-Pro.local>
2020-07-22 21:57:40 +0530

    feta(beta): Optimized images

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,7 +51,8 @@ export default {
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
-    '@nuxtjs/vuetify'
+    '@nuxtjs/vuetify',
+    '@aceforth/nuxt-optimized-images'
   ],
   /*
    ** Nuxt.js modules
